e9c40d397ed73038365fbce8fa576b268db95c64
bgdnvdar <daryabogdahova07.04@gmail.com>
2025-04-28 19:48:18 +0400

    лаба 1

---
+++ labaa1.py
@@ -0,0 +1,49 @@
+def words(wrd):
+    words = {'0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре',
+        '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'}
+    return ' '.join(words[n] for n in wrd)
+
+f = open('txt.txt', 'r')
+file = f.read()
+lst = [x for x in file.split()]
+k = 1
+
+for lexeme in lst:
+    if lexeme.isdigit() and lexeme[0] == '2' and lexeme[1] == '1':
+        c = 1
+        for i in range(1, len(lexeme)):
+            if lexeme[i] == lexeme[i - 1]:
+                c += 1
+            else:
+                if c > k:
+                    print(lexeme, words(str(lexeme[i - 1])), c)
+                c = 1
+        if c > k:
+            print(lexeme, words(str(lexeme[-1])), c)
+
+
+
+
+
+
+
+
+# f = open('txt.txt', 'r')
+# file = f.read()
+# lst = [x for x in file.split()]
+# k = 3
+#
+# for lexeme in lst:
+#     if lexeme.isdigit() and lexeme[0] == '2' and lexeme[1] == '1':
+#         c = 1
+#         for i in range(1,len(lexeme)):
+#             if lexeme[i] == lexeme[i-1]:
+#                 c += 1
+#                 if c > k:
+#                     print(lexeme,words(str(lexeme[i])),c)
+#                     c = 1
+
+
+
+
+
